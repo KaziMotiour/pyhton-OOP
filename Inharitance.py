690f98cd53e16b102b0bb8151b0f03a7a05e4337
Motiour <kmatiour30@gmail.com>
2019-09-01 19:28:52 +0600

    first init

---
+++ Inharitance.py
@@ -0,0 +1,11 @@
+class A():
+    def freautreA1(self):
+        print("Feature A1 is working")
+    def freautreA2(self):
+        print("Feature A2 is working")
+class B(A):
+    def freautreB1(self):
+        print("Feature B1 is working")
+    def freautreB2(self):
+        print("Feature B2 is working")
+b1=B()
