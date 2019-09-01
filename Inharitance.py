9c92f0752f61eed5e02c9497f31ade7ac2c5757c
Motiour <kmatiour30@gmail.com>
2019-09-01 19:49:53 +0600

    inner class

--- Inharitance.py
+++ Inharitance.py
@@ -3,9 +3,21 @@ class A():
         print("Feature A1 is working")
     def freautreA2(self):
         print("Feature A2 is working")
-class B(A):
+class B():
+    def __init__(self):
+        self.c = self.c()
     def freautreB1(self):
         print("Feature B1 is working")
     def freautreB2(self):
         print("Feature B2 is working")
-b1=B()
+    class c():
+        def __init__(self):
+            #self.m = self.m()
+            print("Class c is on")
+            self.sk="sk"
+        def model(self):
+            print("Model : 2018")
+        class m():
+            def __init__(self):
+                print("none")
+B.c.m()
