d7a42b15d2e85c622e8d4c6f8037638233d692bb
Motiour <kmatiour30@gmail.com>
2019-09-02 19:42:26 +0600

    return function from function

--- Decorators.py
+++ Decorators.py
@@ -1,6 +1,24 @@
-s = "Global variable"
+# s = "Global variable"
+#
+# def func():
+#     # global s
+#     # s = 10
+#     # print(s)
+#     my_locla = 40
+#     print(locals())  # return a dictionary
+#
+# func()
 
-def func(s):
-    print(s)
+def hello(name):
+        print("this is a sunny day")
 
-func(s)
+        def greet():
+            return "I'm in greet function"
+
+        def welcome():
+            return "welcome to my function"
+        if name == 'motiour':
+            return greet()
+        else:
+            return welcome()
+print(hello(input()))
